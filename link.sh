de2aa1a100cddd3817bc45608dfd3a3bef80abea
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2022-04-24 16:23:49 +0300

    now uses find -exec to symlink files

--- link.sh
+++ link.sh
@@ -2,13 +2,9 @@
 set -u
 
 search_path=.
+current_path=$(pwd)
 
 # gets all files except git and this file and gets the relative path
-files=$(find "${search_path}" \( -path "${search_path}/.git" -prune -o \
-    -type f \) -a \! \( -name 'link.sh' -o -name ".git*" \) -print0 | \
-    xargs -0 realpath --relative-to=${search_path})
-
-for file in $files
-do
-    ln -sf "$(pwd)/$file" "$HOME/$file"
-done
+find "${search_path}" \( -path "${search_path}/.git" -prune -o \
+    -type f \) -a \! \( -name 'link.sh' -o -name ".git*" \) -exec \
+    ln -sf  "${current_path}/{}" "$HOME/{}" \; -print
