e6149cc729f23f4ff0aba75b4233f6b8ca4937a3
Viktor Horsmanheimo <viktorh@nvidia.com>
2022-05-17 09:25:27 +0300

    automatically download node

--- link.sh
+++ link.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 set -u
 vim_plug_path="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
+NODE_PATH="$HOME/bin/node-v16.15.0-linux-x64"
 
-curl -V
+mkdir -p ~/.config/nvim
+mkdir -p $HOME/bin
 
-if [ $? != 0 ]
+if ! curl -V > /dev/null
 then
     echo "curl not installed"
     exit 1
+fi
+
+if [ ! -d "$NODE_PATH" ]
+then
+    curl -S "https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz" \
+        --output $HOME/bin/node-16.15.tar.xz
+    (
+        cd ~/bin
+        tar xvf "node-16.15.tar.xz"
+        ln -sf "$NODE_PATH/bin/node" .
+        ln -sf "$NODE_PATH/bin/npx" .
+        ln -sf "$NODE_PATH/bin/npm" .
+        rm "$HOME/bin/node-16.15.tar.xz"
+    )
 fi
 
 if [ ! -f "${vim_plug_path}" ]
@@ -16,6 +32,7 @@ then
     sh -c "curl -fLo ${vim_plug_path} --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
+
 search_path=.
 current_path=$(pwd)
 
