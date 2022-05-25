40a7293c8e8313f1b67196d86b5d4d53ebc016f9
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2022-05-25 23:12:49 +0300

    added different architectures

--- link.sh
+++ link.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
-set -u
+# error on both undefined variables and other errors
+set -ue
+
 vim_plug_path="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
-NODE_PATH="$HOME/bin/node-v16.15.0-linux-x64"
+NODE_BASE_URL="https://nodejs.org/dist/v16.15.0"
+ARCH=$(uname -m)
+
+if [ $ARCH == "x86_64" ]; then
+    NODE_FILENAME="node-v16.15.0-linux-x64.tar.xz"
+    NODE_URL="$NODE_BASE_URL/node-v16.15.0-linux-x64.tar.xz"
+elif [ $ARCH == "armv7*" ]; then
+    NODE_FILENAME="node-v16.15.0-linux-armv7l.tar.xz"
+    NODE_URL="$NODE_BASE_URL/"
+elif [ $ARCH == "armv8*" ]; then
+    NODE_FILENAME="node-v16.15.0-linux-arm64.tar.xz"
+    NODE_URL="$NODE_BASE_URL/$NODE_FILENAME"
+else
+    echo "unknown arch"
+    exit 1
+fi
+
+NODE_PATH="$HOME/bin/$NODE_FILENAME"
 
 mkdir -p ~/.config/nvim
 mkdir -p $HOME/bin
@@ -14,15 +33,15 @@ fi
 
 if [ ! -d "$NODE_PATH" ]
 then
-    curl -S "https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz" \
-        --output $HOME/bin/node-16.15.tar.xz
+    curl -S ${NODE_URL} \
+        --output $HOME/bin/$NODE_FILENAME
     (
         cd ~/bin
         tar xvf "node-16.15.tar.xz"
         ln -sf "$NODE_PATH/bin/node" .
         ln -sf "$NODE_PATH/bin/npx" .
         ln -sf "$NODE_PATH/bin/npm" .
-        rm "$HOME/bin/node-16.15.tar.xz"
+        rm "$HOME/bin/$NODE_FILENAME"
     )
 fi
 
