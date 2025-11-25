683df830a46a3ad49d927c1f82756bd393f20eb0
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2025-11-25 13:12:08 +0200

    add hunk commands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -293,6 +207,20 @@ require("lazy").setup({
 				changedelete = { text = "~" },
 			},
 		},
+		config = function(_, opts)
+			require("gitsigns").setup(opts)
+			-- Git hunk keymaps under <leader>g
+			vim.keymap.set({ "n", "v" }, "<leader>ghs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "[G]it [H]unk [S]tage" })
+			vim.keymap.set({ "n", "v" }, "<leader>ghr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "[G]it [H]unk [R]eset" })
+			vim.keymap.set("n", "<leader>ghS", "<cmd>Gitsigns stage_buffer<CR>", { desc = "[G]it [H]unk [S]tage Buffer" })
+			vim.keymap.set("n", "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<CR>", { desc = "[G]it [H]unk [U]ndo Stage" })
+			vim.keymap.set("n", "<leader>ghp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "[G]it [H]unk [P]review" })
+			vim.keymap.set("n", "<leader>ghb", "<cmd>Gitsigns blame_line<CR>", { desc = "[G]it [H]unk [B]lame" })
+			vim.keymap.set("n", "<leader>ghd", "<cmd>Gitsigns diffthis<CR>", { desc = "[G]it [H]unk [D]iff" })
+			vim.keymap.set("n", "<leader>ghD", "<cmd>Gitsigns diffthis ~<CR>", { desc = "[G]it [H]unk [D]iff ~" })
+			vim.keymap.set("n", "]h", "<cmd>Gitsigns next_hunk<CR>", { desc = "Next [H]unk" })
+			vim.keymap.set("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Prev [H]unk" })
+		end,
 	},
 
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -357,7 +285,7 @@ require("lazy").setup({
 			spec = {
 				{ "<leader>s", group = "[S]earch" },
 				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+				{ "<leader>g", group = "[G]it" },
 			},
 		},
 	},
@@ -423,7 +351,37 @@ require("lazy").setup({
 				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
 				--   },
 				-- },
-				-- pickers = {}
+				defaults = {
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							preview_width = 0.6,
+						},
+					},
+					preview = {
+						hide_on_startup = false,
+					},
+				},
+				pickers = {
+					git_commits = {
+						-- Show full commit message in preview
+						preview = {
+							hide_on_startup = false,
+						},
+					},
+					git_bcommits = {
+						-- Show full commit message in preview
+						preview = {
+							hide_on_startup = false,
+						},
+					},
+					git_stash = {
+						-- Show full stash message in preview
+						preview = {
+							hide_on_startup = false,
+						},
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
@@ -473,7 +431,11 @@ require("lazy").setup({
 
 			-- Git-related searches
 			vim.keymap.set("n", "<leader>gc", function()
-				builtin.git_commits()
+				builtin.git_commits({
+					preview = {
+						hide_on_startup = false,
+					},
+				})
 			end, { desc = "[G]it [C]ommits" })
 			vim.keymap.set("n", "<leader>gs", function()
 				builtin.git_status()
@@ -481,6 +443,13 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>gb", function()
 				builtin.git_branches()
 			end, { desc = "[G]it [B]ranches" })
+			vim.keymap.set("n", "<leader>gx", function()
+				builtin.git_stash({
+					preview = {
+						hide_on_startup = false,
+					},
+				})
+			end, { desc = "[G]it Stas[X]" })
 		end,
 	},
 
